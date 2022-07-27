145a5a8544711e6b25149a96cbd3e9b7908c2578
Vincent Scheib <scheib@users.noreply.github.com>
2022-07-26 18:49:37 -0700

    WIP converting physics to angle

--- static/scenes/elfski/js/physics.js
+++ static/scenes/elfski/js/physics.js
@@ -20,8 +20,7 @@ const slerpRatio = 4;
 const accelerationBar = 0.5;
 const maximumSpeed = 0.66;
 const stopBar = 0.75;  // angle at which we are trying to stop
-const initialVec = {x: 1, y: 0};
-
+const initialAngle = Math.PI/2;
 
 export class Character {
   constructor() {
@@ -29,7 +28,8 @@ export class Character {
     this._speed = 0;
     
     /** @private {vec.Vector} */
-    this._vec = initialVec;
+    this._vec = "NO";
+    this._angle = initialAngle;
 
     /** @private {number} */
     this._line = 0;
@@ -60,22 +60,21 @@ export class Character {
    * @return {number} angle in rads
    */
   get angle() {
-    return Math.atan2(this._vec.x, this._vec.y);
+    return this._angle;
   }
 
   /**
    * @return {vec.Vector} angle vector
    */
   get angleVec() {
-    return this._vec;
+    return {x: Math.cos(this._angle), y: Math.sin(this._angle)}
   }
 
   /**
    * @param {number} v angle in rads
    */
   set angle(v) {
-    this._vec.x = Math.cos(v);
-    this._vec.y = Math.sin(v);
+    this._angle = v;
   }
 
   /**
@@ -83,7 +82,7 @@ export class Character {
    */
   crash() {
     this._speed = 0;
-    this._vec = {x: (this._vec.x > 0) ? +1 : -1, y: 0};
+    this._angle = math.sign(this._angle) * initialAngle;
   }
 
   /**
@@ -91,7 +90,7 @@ export class Character {
    */
   reset() {
     this._speed = 0;
-    this._vec = initialVec;
+    this._angle = initialAngle;
     this._line = 0;
   }
 
@@ -106,17 +105,24 @@ export class Character {
       target = vec.unitVec(target);  // deal with bad client data
 
       // player is drifting "up", so retain vector
-      if (target.y < 0 && this._vec.y < stopBar) {
+      if (target.y < 0) {
         target = {
-          x: Math.sign(this._vec.x),
+          x: (target.x > 0) ? 1 : -1,
           y: 0,
         };
       }
 
+      const targetAngle = Math.atan2(target.x, target.y)
       const by = slerpRatio * delta * (maximumSpeed * 2 - this._speed);
-      this._vec = vec.slerp(this._vec, target, by);
+console.log(
+  "angle " + this._angle.toFixed(2),
+  "target " + targetAngle.toFixed(2),
+  "x "+target.x.toFixed(2),
+  "y "+target.y.toFixed(2),
+  "by " + by.toFixed(2));
+      this._angle = this._angle + by * (targetAngle - this._angle);
 
-      this._line = vec.angleBetween(this._vec, target);  // difference between player/goal
+      this._line = Math.abs(targetAngle - this._angle);  // difference between player/goal
     } else {
       this._line = 0;  // character not changing direction
     }
