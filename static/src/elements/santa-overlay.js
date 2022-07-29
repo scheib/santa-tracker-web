f978915714490b15febde69ca1b00d888a65b8c9
Vincent Scheib <scheib@users.noreply.github.com>
2022-07-29 15:59:08 -0700

    working

--- static/src/elements/santa-overlay.js
+++ static/src/elements/santa-overlay.js
@@ -67,7 +67,10 @@ export class SantaOverlayElement extends LitElement {
 
   focusButton() {
     // Ref needed.
-    //this.shadowRoot.querySelector('something something').focus()
+    let button = this.shadowRoot.querySelector('#playagainButton');
+    let button2 = button.shadowRoot.querySelector('button');
+    console.log("focusButton", button, button2);
+    button2.focus();
   }
 
   _dispatchRestart(e) {
@@ -123,7 +126,7 @@ export class SantaOverlayElement extends LitElement {
         <santa-button aria-label=${_msg`play`} color="purple" @click="${this._dispatchResume}" ?hidden=${!this.isPaused}>
           <svg class="icon"><path d="M8 5v14l11-7z"/></svg>
         </santa-button>
-        <santa-button aria-label=${_msg`playagain`} color="purple" @click="${this._dispatchRestart}">
+        <santa-button aria-label=${_msg`playagain`} color="purple" @click="${this._dispatchRestart}" id="playagainButton">
           <svg class="icon"><path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
         </santa-button>
         <santa-button aria-label=${_msg`santasvillage`} color="theme" @click="${this._dispatchHome}" data-action="home">
